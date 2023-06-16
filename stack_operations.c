925c177356a9b8479bf6c73b3a22dc9fd1bf690b
Nehatkr <nehathakur2308@gmail.com>
2023-06-16 09:10:11 +0530

    day3

---
+++ stack_operations.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void push(struct stack *ptr, int val)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack Overflow! Cannot push %d to the stack\n", val);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val; // ptr top is the top most position of the stack
+    }
+}
+
+int pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else
+    {
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int main()
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1; // stack is empty
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("Stack has been created successfully\n");
+
+    printf("Before pushing , Full:%d\n", isFull(sp));
+    printf("Before pushing , Empty:%d\n", isEmpty(sp));
+    push(sp, 56);
+    push(sp, 6);
+    printf("After pushing , Full:%d\n", isFull(sp));
+    printf("After pushing , Empty:%d\n", isEmpty(sp));
+
+    return 0;
+}
